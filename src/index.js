c8f4929a2bf914feca4d3045fa61b600f79e9c42
Joseph Parker <jparker010190@gmail.com>
2018-07-13 19:38:22 +0000

    automatically backed up by learn

--- src/index.js
+++ src/index.js
@@ -10,13 +10,19 @@ const BUTCHER_PRODUCTS = [
 
 // Define these exported classes
 export class OlderCoaster extends Component {
-
   // this function should return the JSX we want the DOM to render
-  // render() {
-  //   return (
-  //     <div>Hello!</div>
-  //   )
-  // }
+   render() {
+     return (
+      <div class="oldercoaster">
+        <p>Two grannies having the time of their life!</p>
+        <p>Passengers:</p>
+        <ul>
+          <li>Agnes</li>
+          <li>Muriel</li>
+        </ul>
+      </div>
+    )
+   }
   //
 }
 
